c4df3f577736f91be60d8431def58879b32a86cc
Sam Machin <sam@sammachin.com>
2025-03-28 09:26:58 +0000

    working call transfer with servertools

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const {createServer} = require('http');
@@ -5,7 +6,7 @@ const {createEndpoint} = require('@jambonz/node-client-ws');
 const server = createServer(app);
 const makeService = createEndpoint({server});
 const logger = require('pino')({level: process.env.LOGLEVEL || 'info'});
-const port = process.env.WS_PORT || 3000;
+const port = process.env.PORT || 3000;
 const routes = require('./lib/api');
 
 app.locals = {
@@ -18,6 +19,11 @@ app.use(express.json());
 app.use('/api', (req, res, next) => {
   next();
 },routes);
+
+// Simple handler for the /final actionHook if it comes over WebHook instead of websocket after the call has been transferred
+app.post('/final', async (req, res) => {
+  res.status(200).json({})
+})
 
 require('./lib/routes')({logger, makeService});
 
